6bb0bacb0758632ca8b06bf4fe44bdc3fa824229
Shivuu2803 <shivanshmishra2606@gmail.com>
2026-07-21 23:53:21 +0530

    hide Recommendations section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Writing from "@/components/Writing";
-import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -22,7 +21,6 @@ export default function Home() {
         <Experience tinted />
         <Projects />
         <Writing tinted />
-        <Testimonials />
         <Contact />
       </main>
       <Footer />
